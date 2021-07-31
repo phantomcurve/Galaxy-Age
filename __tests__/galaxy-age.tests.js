236bf120aa749330e56453d6ff18f93c2a987f4a
Tim Roth <timdroth@gmail.com>
2021-07-31 13:46:58 -0700

    write passing mercuryAge test

--- __tests__/galaxy-age.tests.js
+++ __tests__/galaxy-age.tests.js
@@ -5,4 +5,11 @@ describe ('galacticAge', () => {
       const newAge = new galacticAge();
       expect (newAge).toEqual(new galacticAge);
     });
-})
+
+    test('should convert newAge to Mercury years', () => {
+      let earthAge = 36
+      let mercuryAge = earthAge * 0.24;
+      expect (mercuryAge).toEqual(8.64);
+    });
+});
+
